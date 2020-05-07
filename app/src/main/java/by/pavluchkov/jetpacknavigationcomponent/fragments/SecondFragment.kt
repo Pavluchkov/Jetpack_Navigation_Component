52cd536819960ae0c35220052f8da91d47f4d549
Anton Pavluchkov <pavluchkov1501@gmail.com>
2020-05-07 20:40:14 +0300

    Create fragments

---
+++ app/src/main/java/by/pavluchkov/jetpacknavigationcomponent/fragments/SecondFragment.kt
@@ -0,0 +1,20 @@
+package by.pavluchkov.jetpacknavigationcomponent.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import by.pavluchkov.jetpacknavigationcomponent.R
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+}
