658d444f3635e5c5438385931c985643567d41f8
Anton Pavluchkov <pavluchkov1501@gmail.com>
2020-05-08 18:24:40 +0300

    Implemented passing argument to ShowFragment from ThirdFragment

--- app/src/main/java/by/pavluchkov/jetpacknavigationcomponent/fragments/ShowPersonFragment.kt
+++ app/src/main/java/by/pavluchkov/jetpacknavigationcomponent/fragments/ShowPersonFragment.kt
@@ -4,17 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import by.pavluchkov.jetpacknavigationcomponent.Person
 import by.pavluchkov.jetpacknavigationcomponent.R
 
 class ShowPersonFragment : Fragment() {
+
+    var person: Person? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_show_person, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.text_showPerson_fragment).text = person.toString()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        person = arguments?.getParcelable("myPerson")
+    }
 }
--- app/src/main/java/by/pavluchkov/jetpacknavigationcomponent/fragments/ThirdFragment.kt
+++ app/src/main/java/by/pavluchkov/jetpacknavigationcomponent/fragments/ThirdFragment.kt
@@ -1,14 +1,18 @@
 package by.pavluchkov.jetpacknavigationcomponent.fragments
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import by.pavluchkov.jetpacknavigationcomponent.Person
 import by.pavluchkov.jetpacknavigationcomponent.R
+import kotlinx.android.synthetic.main.fragment_third.*
 
 class ThirdFragment : Fragment(), View.OnClickListener {
 
@@ -31,10 +35,20 @@ class ThirdFragment : Fragment(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         when (v!!.id) {
-            //R.id.btn_confirm ->
-            R.id.btn_show_person -> navController.navigate(R.id.action_thirdFragment_to_showPersonFragment)
+            R.id.btn_confirm,
+            R.id.btn_show_person -> {
+                if (!TextUtils.isEmpty(et_personName_thirdFragment.text.toString())
+                    && !TextUtils.isEmpty(et_personAge_thirdFragment.text.toString())
+                ) {
+                    val person = Person(
+                        et_personName_thirdFragment.text.toString(),
+                        et_personAge_thirdFragment.text.toString().toInt()
+                    )
+                    val bundle = bundleOf("myPerson" to person)
+                    navController.navigate(R.id.action_thirdFragment_to_showPersonFragment, bundle)
+                }
+            }
         }
+
     }
-
-
 }
