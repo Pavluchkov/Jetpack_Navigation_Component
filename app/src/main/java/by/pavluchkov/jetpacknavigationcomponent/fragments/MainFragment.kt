2766f15c0696c20b0ef6da9ab107f0ab2b4594a9
Anton Pavluchkov <pavluchkov1501@gmail.com>
2020-05-08 16:31:06 +0300

    implemented navigation inside fragments MainFragment and ThirdFragment

--- app/src/main/java/by/pavluchkov/jetpacknavigationcomponent/fragments/MainFragment.kt
+++ app/src/main/java/by/pavluchkov/jetpacknavigationcomponent/fragments/MainFragment.kt
@@ -4,16 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import by.pavluchkov.jetpacknavigationcomponent.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.button_first).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button_second).setOnClickListener(this)
+        view.findViewById<Button>(R.id.button_third).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.button_first -> navController.navigate(R.id.action_mainFragment_to_firstFragment)
+            R.id.button_second -> navController.navigate(R.id.action_mainFragment_to_secondFragment)
+            R.id.button_third -> navController.navigate(R.id.action_mainFragment_to_thirdFragment)
+
+        }
     }
 
 }
--- app/src/main/java/by/pavluchkov/jetpacknavigationcomponent/fragments/ThirdFragment.kt
+++ app/src/main/java/by/pavluchkov/jetpacknavigationcomponent/fragments/ThirdFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import by.pavluchkov.jetpacknavigationcomponent.R
 
-class ThirdFragment : Fragment() {
+class ThirdFragment : Fragment(), View.OnClickListener {
+
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +21,20 @@ class ThirdFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_third, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.btn_confirm).setOnClickListener(this)
+        view.findViewById<Button>(R.id.btn_show_person).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            //R.id.btn_confirm ->
+            R.id.btn_show_person -> navController.navigate(R.id.action_thirdFragment_to_showPersonFragment)
+        }
+    }
+
 
 }
